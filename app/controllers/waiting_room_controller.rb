31ea6dba53da38e399867076a2cd61c5a9c20f3c
Alex <alexander.eklund@auctionet.com>
2023-07-06 14:32:17 +0200

    Add rudimentary clear waiting list button

--- app/controllers/waiting_room_controller.rb
+++ app/controllers/waiting_room_controller.rb
@@ -5,30 +5,46 @@ class WaitingRoomController < ApplicationController
   def index
     @current_user = User.find_by(id: session[:user_id])
     @waiting_list = redis.lrange('waiting_list', 0, -1)
+    @waiting_list_count = redis.llen('waiting_list')
+
+    puts "WAITING LIST COUNT (index): #{@waiting_list_count}"
 
     if !@waiting_list.include?(@current_user.id.to_s)
       enter_waiting_list
     end
 
+    waiting_list = redis.lrange('waiting_list', 0, -1)
+
+    puts "WAITING LIST COUNT (index): #{waiting_list.count}"
+
     retrieve_waiting_list
   end
 
   def enter_waiting_list
+    puts "Added to WAITING LIST (enter_waiting_list): #{@current_user.email}"
     redis.lpush('waiting_list', @current_user.id)
+
   end
 
   def exit_waiting_list
 
+    userId = params[:userId].to_s
 
-puts "OKOKOKOK #{@current_user.id}"
-   # userId = params[:userId]
-   # redis.lrem('waiting_list', 0, userId)
+    if !redis.lrange('waiting_list', 0, -1).include?(userId)
+      puts "Removed from WAITING LIST (enter_waiting_list): #{@current_user.email}"
+      puts "WAITING LIST COUNT (exit_waiting_list): #{waiting_list.count}"
+      redis.lrem('waiting_list', 0, userId)
+    end
+    waiting_list = redis.lrange('waiting_list', 0, -1)
+
+
 
     render json: { message: "Exited waiting list successfully" }
   end
 
   def clear_waiting_list
     redis.del('waiting_list')
+    render json: { waitingListCount: @waiting_list_count }
   end
 
   def retrieve_waiting_list
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   post '/lobby/create', to: 'lobby#create', as: 'lobby_create'
   post '/lobby/login', to: 'lobby#login', as: 'lobby_login'
   post '/waiting_room_controller/exit_waiting_list', to: 'waiting_room#exit_waiting_list'
+  get '/clear_waiting_list', to: 'waiting_room#clear_waiting_list'
 
   get 'waiting_room', to: 'waiting_room#index'
 
